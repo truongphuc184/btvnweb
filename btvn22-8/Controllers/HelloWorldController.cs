b08cf6df6845f0eb60c1979b2e35cdadadbe4088
DELL <DELL@DESKTOP-7KGQ86Q>
2025-08-28 22:17:02 +0700

    Add project files.

---
+++ btvn22-8/Controllers/HelloWorldController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata;
+
+namespace btvnweb.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Welcome()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ btvn22-8/Controllers/HiWorldController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace btvnweb.Controllers
+{
+    public class HiWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Welcome()
+        {
+            return View();
+        }
+    }
+}
